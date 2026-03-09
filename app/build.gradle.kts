38cf00fd23cdfb470dca3210f97071c751011dc1
zawar <f25-bscs-0039@qu.edu.pk>
2026-03-09 18:31:29 +0500

    change SDK
    changed SDk version to 21 using gemini because it was casing the app to crash (compiling error)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.rps"
-        minSdk = 16
+        minSdk = 23
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
